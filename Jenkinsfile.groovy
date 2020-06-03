d4e748a7afcd360927cc6ac663b0bc53ed77a6da
Chris <ceinfalt1@gmail.com>
2020-06-03 12:27:53 -0500

    added local try catch loops

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,6 +19,7 @@ def buildNumber = ${env.BUILD_NUMBER}  //Global Variable within Jenkins
 def branchName = ${env.BRANCH_NAME}  //Global Variable within Jenkins
 
 //error variables
+def prepareFailure = false
 def buildFailure = false
 def testFailure = false
 def deployFailure = false
@@ -27,10 +28,20 @@ node (slave) {
     try {
     stage('Prepare Environment'){
         //clone
+            try {
+                echo "Prepare Environment"
+            }catch{
+                prepareFailure = true;
+                throw err
     }
     stage('Build'){
         echo "Building..."
-        //build
+        //build to 2 servers
+            try {
+                echo "Build"
+            }catch{
+                buildFailure = true;
+                throw err
     }
     stage('Test'){
         echo "Not Testing Yet!"
@@ -38,8 +49,15 @@ node (slave) {
     stage('Deploy'){
         echo "Deploying..."
         //deploy to 2 servers
+            try {
+                echo "Deploy Environment"
+            }catch{
+                deployFailure = true;
+                throw err
     } catch (err)(
         if (buildFailure) {
+            echo "Prepare Failure"
+        } else if (buildFailure) {
             echo "Build Failure"
         } else if (testFailure) {
             echo "Test Failure"
