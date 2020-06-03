7b49711280e7b32a4e87dc11a562103d9f53bbb3
dmcglynn31 <34779584+dmcglynn31@users.noreply.github.com>
2020-06-03 10:19:31 -0500

    Added placeholders for recommended variables from Hybris repo

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,20 @@
 
 //set variables
 def projectName = "hybrisHomework"
+//def nexusCredID = "nexusCredIDPlaceholder"
+//def nexusURL = "nexusURLPlaceholder"
+//def nexusRepo = "nexusURLPlaceholder"
+//def nexusOrg = "nexusURLPlaceholder"
+//def artifactName = "nexusURLPlaceholder"
+//def versionID = "nexusURLPlaceholder"
+//def server = "nexusURLPlaceholder"
+//def appEnvironment = "nexusURLPlaceholder"
+//def hybrisComponent = "nexusURLPlaceholder"
+//def artifactTag = "nexusURLPlaceholder"
+//def sonarEnv = "nexusURLPlaceholder"
+def workspace = ${env.WORKSPACE} //Global Variable within Jenkins
+def buildNumber = ${env.BUILD_NUMBER}  //Global Variable within Jenkins
+def branchName = ${env.BRANCH_NAME}  //Global Variable within Jenkins
 
 node (slave) {
     stage('Prepare Environment'){
